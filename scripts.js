38dc4a6ca9918331ee115c451df03fc53683985f
Pollyana Masiero Torquato <113457708+pollyanamasiero@users.noreply.github.com>
2023-07-27 12:45:47 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,28 +1,29 @@
 function calculateTip(event) {
-  event.preventDefault();
-  let bill = document.getElementById('bill').value;
-  let servicequality = document.getElementById('servicequality').value;
-  let numOfPeople = document.getElementById('people').value;
+    event.preventDefault();
+    let bill = document.getElementById('bill').value;
+    let serviceQual = document.getElementById('serviceQual').value;
+    let numOfPeople = document.getElementById('people').value;
 
-  if (bill == '' | servicequality == 0){
-    alert("Por favor, preenche os valores")
-    return;
-  }
+    if(bill == '' | serviceQual == 0) {
+        alert("Por favor, preenche os valores")
+        return;
+    }
 
-  if (numOfPeople == "" | numOfPeople <= 1) {
-    numOfPeople = 1;
-    document.getElementById('each').style.display = "none"
-  } else {
-    document.getElementById('each').style.display = "block"
-  }
+    if(numOfPeople == '' | numOfPeople <= 1) {
+        numOfPeople = 1;
+        document.getElementById('each').style.display = "none"
+    } else {
+        document.getElementById('each').style.display = "block"
+    }
 
-  let total = (bill * servicequality) / numOfPeople;
-  total = total.toFixed(2);
-  document.getElementById('tip').innerHTML = total;
-  document.getElementById('totaltip').style.display = "block";
+    let total = (bill * serviceQual) / numOfPeople;
+    total = total.toFixed(2);
+    document.getElementById('tip').innerHTML = total;
+    document.getElementById('totalTip').style.display = "block";
+
 }
 
-document.getElementById('totaltip').style.display = "none";
+document.getElementById('totalTip').style.display = "none";
 document.getElementById('each').style.display = "none";
 
-document.getElementById('tipsform').addEventListener('submit', calculateTip);
+document.getElementById('tipsForm').addEventListener('submit', calculateTip);
